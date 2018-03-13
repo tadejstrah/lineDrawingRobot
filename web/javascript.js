745af26d4a9eb15a7d9b379cd43557780291a4b1
Gregor Kržmanc <gregor.krzmanc@gmail.com>
2018-03-13 22:19:07 +0100

    risanje

--- web/javascript.js
+++ web/javascript.js
@@ -12,82 +12,100 @@ var Ys = []
 var lineLen = 2000;
 
 var currentLineLength = 0; 
+var drawing = false;
 
 window.onload = function() {
-  canvas = document.getElementById('myCanvas');
-  canvasContext = canvas.getContext('2d');
-  
-  var FPS = 60;
+    canvas = document.getElementById('myCanvas');
+    canvasContext = canvas.getContext('2d');
 
-  setInterval(function(){drawEverything();},1000/FPS);
-  
+    var FPS = 60;
 
-  canvas.addEventListener('mousemove',
-    function(evt){
-      var mousePos = calculateMousePos(evt);
-      Xcurrent = mousePos.x;
-      Ycurrent = mousePos.y;
+    setInterval(function(){drawEverything();},1000/FPS);
+
+
+    canvas.addEventListener('mousemove',
+                            function(evt){
+        if(currentLineLength >= lineLen || drawing == false) return;
+        var mousePos = calculateMousePos(evt);
+        Xcurrent = mousePos.x;
+        Ycurrent = mousePos.y;
     }
-    )
-  canvas.addEventListener('mousedown', handleMouseClick);
-  }
+                           )
+    canvas.addEventListener('mousedown', function(evt) {
+        handleMouseClick(evt);
+
+    });
+    canvas.addEventListener('mouseup', function() {
+        currentLineLength = lineLen;
+        drawing=false;
+    });
+}
 
 
 
 function calculateMousePos(evt) {
-  var rect = canvas.getBoundingClientRect();
-  var root = document.documentElement;
-  var mouseX = evt.clientX - rect.left - root.scrollTop;
-  var mouseY = evt.clientY - rect.top - root.scrollTop;
-  Xs.push(Xcurrent);
-  Ys.push(Ycurrent); 
-  return{
-    x:mouseX,
-    y:mouseY
+    var rect = canvas.getBoundingClientRect();
+    var root = document.documentElement;
+    var mouseX = evt.clientX - rect.left - root.scrollTop;
+    var mouseY = evt.clientY - rect.top - root.scrollTop;
+    Xs.push(Xcurrent);
+    Ys.push(Ycurrent); 
+    return{
+        x:mouseX,
+        y:mouseY
+    }
 }
-}
-
 
 function chooseLength() {
     var length1 = prompt("določi dolžino (nekje med 500 in 10k)");
     if (length1 != null) {
-       lineLen = length1;
+        lineLen = length1;
         canvasContext.fillStyle = 'lightblue';
-       canvasContext.fillRect(0,0,canvas.width,canvas.height);
-       currentLineLength = 0;
-       Xs = [];
-       Ys = [];
+        canvasContext.fillRect(0,0,canvas.width,canvas.height);
+        currentLineLength = 0;
+        Xs = [];
+        Ys = [];
 
 
     }
 }
 
-
 function drawEverything(){
-  if(Xprev != Xcurrent && currentLineLength < lineLen){
+    if(Xprev != Xcurrent && currentLineLength < lineLen){
 
-  currentLineLength += Math.sqrt((Yprev - Ycurrent)*(Yprev - Ycurrent) + (Xprev - Xcurrent)*(Xprev - Xcurrent));
-  console.log(currentLineLength);
+        currentLineLength += Math.sqrt((Yprev - Ycurrent)*(Yprev - Ycurrent) + (Xprev - Xcurrent)*(Xprev - Xcurrent));
+        console.log(currentLineLength);
 
-  canvasContext.stroke();
-  canvasContext.beginPath();
-  canvasContext.moveTo(Xprev,Yprev);
-  canvasContext.lineTo(Xcurrent,Ycurrent);
-  canvasContext.stroke();
+        canvasContext.stroke();
+        canvasContext.beginPath();
+        canvasContext.moveTo(Xprev,Yprev);
+        canvasContext.lineTo(Xcurrent,Ycurrent);
+        canvasContext.stroke();
 
-  Xprev = Xcurrent;
-  Yprev = Ycurrent;
+        Xprev = Xcurrent;
+        Yprev = Ycurrent;
+    }
+    return
 }
-  return
-}
 
-function handleMouseClick(){
-  console.log(Xs)
-  console.log(Ys)
-  return
+function handleMouseClick(evt){
+
+    console.log(Xs)
+    console.log(Ys)
+    currentLineLength = 0;
+
+    Xs = [];
+    Ys = [];
+    canvasContext.fillStyle = 'lightblue';
+    canvasContext.fillRect(0,0,canvas.width,canvas.height);
+    drawing=true;
+    var tmp = calculateMousePos(evt);
+    Xprev=tmp.x;Yprev=tmp.y;
+    Xcurrent=Xprev;Ycurrent=Yprev;
+    return
 }
 
 function sendData(){
-  console.log("tuki dodej funkcijo ki pošlje POST request RPI-ju");
-  return
+    console.log("tuki dodej funkcijo ki pošlje POST request RPI-ju");
+    return
 }
