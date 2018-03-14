a86835110fce17ba9469c1d4366b139a0e787a44
tadejstrah <tadej.strah@gmail.com>
2018-03-14 10:08:44 +0100

    test

--- web/test.py
+++ web/test.py
@@ -6,8 +6,8 @@ tabela=[]
 a=0
 turtle.pu()
 turtle.goto(0, 0)
-while a <= 10000:
-    x=random.randint(-100, 100) #ker je naš uporabnik pač moderni umetnik
+while a <= 5000:
+    x=random.randint(-100, 100) #ker je nas uporabnik pac moderni umetnik
     y=random.randint(-100, 100)
     a = a + math.sqrt((x*x) + (y*y))
     tabela.append({"x":x, "y":y})
