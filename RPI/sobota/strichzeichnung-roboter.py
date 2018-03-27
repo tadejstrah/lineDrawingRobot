22ac08eeb3a119c50d4d3ad526d351d2d3b280bc
Gregor <gregor.krzmanc@gmail.com>
2018-03-27 10:26:02 +0000

    sobotne delavnice

---
+++ RPI/sobota/strichzeichnung-roboter.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask import request
+from test3 import speed
+app = Flask(__name__)
+
+@app.route('/a', methods=['POST'])
+def home():
+    data = request.get_json()
+    data = data["result"]["action"]
+    print("Request_action: ", data)
+    if(data=='levo'):
+        speed(0,500,1,1,4)
+    elif(data=='desno'):
+        speed(500,0,0,0,4)
+    elif(data=='naprej'):
+        speed(500,500,0,1,5)
+    elif(data=='nazaj'):
+        speed(500,500,1,0,5)
+    return "yo!"
+
+if __name__ == "__main__":
+    app.run()
---
+++ RPI/sobota/test3.py
@@ -0,0 +1,58 @@
+from time import sleep
+from time import time
+from threading import Thread
+from gpiozero import LED
+
+enable = LED(12)
+enable.on()
+
+
+
+dleft = LED(25)
+dleft.on()
+dright = LED(19)
+dright.off()
+left = LED(21)
+right = LED(26)
+
+def step_right(freq,stop):
+    start = time()
+    if(freq==0): return
+    while (time()-start < stop):
+        right.on()
+        sleep(1/(2*freq))
+        right.off()
+        sleep(1/(2*freq))
+
+def step_left(freq,stop):
+    start = time()
+    if(freq==0): return
+    while (time()-start < stop):
+        left.on()
+        sleep(1/(2*freq))
+        left.off()
+        sleep(1/(2*freq))
+def speed(l,r,ld,rd,time):
+    if(ld): dleft.on()
+    else: dleft.off()
+    if(rd): dright.on()
+    else: dright.off()
+    t = Thread(target=step_left,args=(l,time))
+    r = Thread(target=step_right,args=(r,time))
+    t.start()
+    r.start()
+    sleep(time)
+    return
+'''
+    def setSpeed(left,right):
+    global freqleft,freqright
+    freqleft=left
+    freqright = right
+
+def test(state,time):
+    if(state==1):
+        setSpeed(500,500)
+        start()
+        
+        sleep(time)
+    setSpeed(0,0)'''
